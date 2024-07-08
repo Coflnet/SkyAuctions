a4a7129e9d66b2f1200162f15992a33071c9ba57
Äkwav <ekwav@coflnet.com>
2024-07-08 16:01:00 +0200

    order bids by amount first

--- Services/RestoreService.cs
+++ Services/RestoreService.cs
@@ -117,7 +117,7 @@ public class RestoreService
                     bid.Timestamp = DateTime.SpecifyKind(bid.Timestamp, DateTimeKind.Utc);
                 }
             }
-            archivedObj.Bids = archivedObj.Bids.OrderBy(b => b.Timestamp).ThenBy(b => b.Amount).ToList();
+            archivedObj.Bids = archivedObj.Bids.OrderBy(b => b.Amount).ThenBy(b => b.Timestamp).ToList();
             archivedObj.Enchantments = archivedObj.Enchantments.OrderBy(e => e.Type).ToList();
             archivedObj.FlatenedNBT = archivedObj.FlatenedNBT.OrderBy(n => n.Key).ToDictionary(n => n.Key, n => n.Value);
         }
