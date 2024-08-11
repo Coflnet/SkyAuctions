10172646f4a0673d27d9516d95660d3dafa9a6bb
Äkwav <ekwav@coflnet.com>
2024-08-11 23:56:11 +0200

    take deleting out of service

--- Services/DeletingBackGroundService.cs
+++ Services/DeletingBackGroundService.cs
@@ -27,8 +27,8 @@ public class DeletingBackGroundService : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        if (System.Net.Dns.GetHostName().Contains("ekwav"))
-            return;
+        logger.LogInformation("Deleting Background Service is disabled.");
+        return;
         while (!stoppingToken.IsCancellationRequested)
         {
             await Delete();
