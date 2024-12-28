5dd8b8d3bb3f1da4bc3f42a6ec3402de99cf4d2e
Äkwav <ekwav@coflnet.com>
2024-12-28 17:15:50 +0100

    update client generation

--- Client/generate.sh
+++ Client/generate.sh
@@ -1,12 +1,13 @@
-VERSION=0.1.1
+VERSION=0.1.2
+PACKAGE_NAME=Coflnet.Sky.Auctions.Client
 
 docker run --rm -v "${PWD}:/local" --network host -u $(id -u ${USER}):$(id -g ${USER})  openapitools/openapi-generator-cli generate \
 -i http://localhost:5031/swagger/v1/swagger.json \
 -g csharp \
--o /local/out --additional-properties=packageName=Coflnet.Sky.Auctions.Client,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
+-o /local/out --additional-properties=packageName=$PACKAGE_NAME,packageVersion=$VERSION,licenseId=MIT,targetFramework=net6.0
 
 cd out
-path=src/Coflnet.Sky.Auctions.Client/Coflnet.Sky.Auctions.Client.csproj
+path=src/$PACKAGE_NAME/$PACKAGE_NAME.csproj
 sed -i 's/GIT_USER_ID/Coflnet/g' $path
 sed -i 's/GIT_REPO_ID/SkyAuctions/g' $path
 sed -i 's/>OpenAPI/>Coflnet/g' $path
@@ -14,4 +15,5 @@ sed -i 's@annotations</Nullable>@annotations</Nullable>\n    <PackageReadmeFile>
 sed -i '34i    <None Include="../../../../README.md" Pack="true" PackagePath="\"/>' $path
 
 dotnet pack
-cp src/Coflnet.Sky.Auctions.Client/bin/Release/Coflnet.Sky.Auctions.Client.*.nupkg ..
+cp src/$PACKAGE_NAME/bin/Release/$PACKAGE_NAME.*.nupkg ..
+dotnet nuget push ../$PACKAGE_NAME.$VERSION.nupkg --api-key $NUGET_API_KEY --source "nuget.org" --skip-duplicate
